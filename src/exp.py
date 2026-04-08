dfa8300d2c00c0965a792cc0accc5e0f90407423
GMOC <gmartinsoc@gmail.com>
2026-04-08 16:27:39 -0300

    add precision and acc metrics and iris support.

--- src/exp.py
+++ src/exp.py
@@ -34,27 +34,32 @@ def data_in(arquivo):
     #return features, labels 
 
 
-def exp_pred(X_test,y_test, model, prediction_time,bits_encoding):
+def exp_pred(X,y_test, model, prediction_time,bits_encoding):
 
+    
     if device.type == "cuda":
         torch.cuda.synchronize()
-    X=torch.tensor(X_test.values).to(device)
+    #X_test=torch.tensor(X_test.values).to(device)
     
     X,t=encoding(X,prediction_time,bits_encoding)
-        
+    prediction_time+t
+
     start_prediction = time.perf_counter()
     with torch.no_grad():
         predictions = model.predict(X)
-
+    
+    
     prediction_time += time.perf_counter() - start_prediction
-    
-    
-    return pd.DataFrame({"predictions":predictions, "labels":y_test}),prediction_time+t
-
+    X=None
+    #try:
+    return predictions.tolist(),prediction_time
+    # except:
+    #     print(f'dataframe error {type(predictions.tolist())} {type(y_test.values)}')
+    #     return pd.DataFrame({"predictions":predictions.tolist(), "labels":y_test.values.tolist()}),prediction_time
 
    
 
-def exp_train(X_train,  y_train,training_time,model):
+def exp_train(X_train, y_train,training_time,model):
 
 
     # ==================================================
@@ -66,8 +71,10 @@ def exp_train(X_train,  y_train,training_time,model):
     start_training = time.perf_counter()
 
     with torch.no_grad():
-        model.fit(X_train, y_train)
-
+        if args.model=='wis' and args.bleaching:
+            model.fit(X_train, y_train)
+        else:
+            model.fit_bleach(X_train, y_train)
     training_time += time.perf_counter() - start_training
     return training_time, model
 
@@ -140,25 +147,36 @@ training_time=0
 features=pd.DataFrame()
 labels=pd.DataFrame()
 
-pasta_csv=args.data
-for arquivo in sorted(listdir(pasta_csv),key=lambda s: (len(s), s)):
-    file_path=join(pasta_csv, arquivo)
-    print(file_path+'                            ', end="\r")
-    if isfile(file_path) and ".pkl" in arquivo:
-        f=data_in(file_path)
-        if "label" in arquivo:
-           labels = pd.concat([labels, f], ignore_index=True)
-        if "feature" in arquivo:
-           features = pd.concat([features, f], ignore_index=True)
-        
-        
-f=None
-# features =data_in('data/feature_2.pkl')
-# labels = data_in('data/label_2.pkl')
+if args.data=='iris':
+    
+    from torchwnn.datasets.iris import Iris
+    iris = Iris()
 
-labels=labels.astype('int')
+    features = iris.features
 
-num_classes=pd.read_pickle('./data/n_classes.pkl').squeeze()
+    labels= pd.DataFrame(iris.labels.tolist(), columns=['labels'])
+    print(labels.labels.unique())
+    labels=labels.astype('int')
+    num_classes=iris.num_classes
+    print (type(features),type(labels))
+
+else:
+    pasta_csv=args.data
+    for arquivo in sorted(listdir(pasta_csv),key=lambda s: (len(s), s)):
+        file_path=join(pasta_csv, arquivo)
+        print(file_path+'                            ', end="\r")
+        if isfile(file_path) and ".pkl" in arquivo:
+            f=data_in(file_path)
+            if "label" in arquivo:
+                labels = pd.concat([labels, f], ignore_index=True)
+            if "feature" in arquivo:
+                features = pd.concat([features, f], ignore_index=True)
+            
+    f=None
+    
+    labels=labels.astype('int')
+    num_classes=pd.read_pickle('./data/n_classes.pkl').squeeze()
+
 #==================================================
 #End of Loading dataset
 #==================================================
@@ -185,12 +203,15 @@ l_data=len(features.index)
 #thermometer size
 bit_t=args.thermometer_size
 
+train_features=pd.DataFrame()
+train_labels=pd.DataFrame()
 test_features=pd.DataFrame()
-test_labels=pd.Series()
+test_labels=pd.DataFrame()
 
 er=0
 for i in range(0, l_data, batch_size):
-    if l_data >=i+batch_size: 
+    #print(i,l_data,batch_size)
+    if l_data >=i+batch_size-1: 
         # ==================================================
         # Iteranting dataset through mini batch
         # The mini batch is splited into train and test data
@@ -198,8 +219,8 @@ for i in range(0, l_data, batch_size):
         
         #X = torch.tensor(features[i:i+batch_size].values).to(device)
         #y = torch.tensor(list(labels[i:i+batch_size].squeeze())).to(device)
-        X = features[i:i+batch_size]#.values
-        y = labels[i:i+batch_size]
+        X = features[i:i+batch_size-1]#.values
+        y = labels[i:i+batch_size-1]
         try:
 
             X_train_raw, X_test_raw, y_train, y_test = train_test_split(
@@ -207,8 +228,11 @@ for i in range(0, l_data, batch_size):
             )
             X,y=None,None
 
+            train_features=pd.concat([test_features,X_test_raw], ignore_index=True)
+            train_labels=pd.concat([test_labels,y_test], ignore_index=True)
             test_features=pd.concat([test_features,X_test_raw], ignore_index=True)
             test_labels=pd.concat([test_labels,y_test], ignore_index=True)
+            X_train_raw, X_test_raw, y_train, y_test=None,None,None,None
         except:
             er+=1
             continue
@@ -226,45 +250,90 @@ for i in range(0, l_data, batch_size):
             )
             X,y=None,None
 
+            train_features=pd.concat([test_features,X_test_raw], ignore_index=True)
+            train_labels=pd.concat([test_labels,y_test], ignore_index=True)
             test_features=pd.concat([test_features,X_test_raw], ignore_index=True)
             test_labels=pd.concat([test_labels,y_test], ignore_index=True)
+            X_train_raw, X_test_raw, y_train, y_test=None,None,None,None
+
         except:
             er+=1
             continue
         
-    X_train_raw=torch.tensor(X_train_raw.values).to(device)
-    y_train=torch.tensor(list(y_train.squeeze())).to(device)
-    encoding_time,training_time,model = main(X_train_raw,y_train,encoding_time,training_time,model,bit_t)
-    X_train_raw,y_train=None,None
+train_features=torch.tensor(train_features.values).to(device)
+train_labels=torch.tensor(list(train_labels.squeeze())).to(device)
+encoding_time,training_time,model = main(train_features,train_labels,encoding_time,training_time,model,bit_t)
+X_train_raw,y_train=None,None
 
-print(' start testing                       ', end="\r")
+#test_labels.dropna( inplace=True)
+test_features=torch.tensor(test_features.values).to(device)
+print(' start testing                       ')
 results,prediction_time=exp_pred(test_features,test_labels, model, prediction_time, bit_t)
 
+test_labels['pred']= results
+#print('acc: ', test_labels.loc[test_labels.pred==test_labels.label].pred.count()/test_labels.pred.count())
 
 #==================================================================
 # Writing results
 #==================================================================
 d=l_data/batch_size
+print(f'N de pontos {l_data}')
 model_size=sys.getsizeof(model)
-print (f'mAP {precision_score(list(results['predictions'].squeeze()), list(results['labels'].squeeze()))}')
-print (f'Model size {model_size} bytes.')
-print (f"\nEncoding time {encoding_time} seconds")
-print (f"Training time {training_time} seconds")
-print (f"Prediction time {prediction_time} seconds")
-print (f"\n {er} | {er/d} de batchs perdidos.")
-
 
 mypath = args.output+f'+{datetime.now().strftime("%Y%m%d_%H%M%S")}'
 if not isdir(mypath):
    makedirs(mypath)
 
-with open(mypath+"/time.csv", "w", encoding="utf-8") as f:
-    f.write("encoding_time,training_time,prediction_time,model_size\n")
-    f.write(f"{encoding_time},{training_time},{prediction_time},{f'{model_size}'}")
+with open(mypath+"/config.csv", "w", encoding="utf-8") as f:
+    f.write(f"{args}")
 
 with open(mypath+"/model.pkl", 'wb') as file:
     pkl.dump(model, file)
-with open(mypath+"/results_df.pkl", 'wb') as file:
-    pkl.dump(results, file)
+
 with open(mypath+"/test_features_df.pkl", 'wb') as file:
     pkl.dump(test_features, file)
+
+# with open(mypath+"/results_df.pkl", 'wb') as file:
+#     pkl.dump(pd.DataFrame({"predictions":results,"labels":test_labels}) , file)
+
+
+print (f'Model size {model_size} bytes.')
+print (f"\nEncoding time {encoding_time} seconds")
+print (f"Training time {training_time} seconds")
+print (f"Prediction time {prediction_time} seconds")
+print (f"\n {er} | {er/d} de batchs perdidos.")
+with open(mypath+"/results_df.pkl", 'wb') as file:
+    pkl.dump(test_labels , file)
+try:
+    p=precision_score(results, test_labels.labels.values.tolist(),average='weighted')
+    
+    with open(mypath+"/time.csv", "w", encoding="utf-8") as f:
+        f.write("encoding_time,training_time,prediction_time,model_size,mAP\n")
+        f.write(f"{encoding_time},{training_time},{prediction_time},{model_size},{p}")
+    print (f'mAP {p}')    
+    if args.data == 'iris':
+    
+        print('acc: ', test_labels.loc[test_labels.pred==test_labels.labels].pred.count()/test_labels.pred.count())      
+    else:
+    
+        print('acc: ', test_labels.loc[test_labels.pred==test_labels.label].pred.count()/test_labels.pred.count())
+
+    
+    # print(test_labels.head(15))
+    # print (results)
+    # print(test_labels)
+except Exception as e :
+    print(e)
+    #print(test_labels.head(15))
+    if args.data == 'iris':
+        print (type(results),type(test_labels.labels.values.tolist()))
+        print('acc: ', test_labels.loc[test_labels.pred==test_labels.labels].pred.count()/test_labels.pred.count())      
+    else:
+        print (type(results),type(test_labels.label.values.tolist()))
+        print('acc: ', test_labels.loc[test_labels.pred==test_labels.label].pred.count()/test_labels.pred.count())
+    with open(mypath+"/time.csv", "w", encoding="utf-8") as f:
+        f.write("encoding_time,training_time,prediction_time,model_size\n")
+        f.write(f"{encoding_time},{training_time},{prediction_time},{model_size}")
+   
+    # print (results)
+    #print(test_labels)
